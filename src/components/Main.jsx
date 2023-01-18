b45bbf4fbda626f5b212b533432c8d1f9cd70a6f
haneebunny <re____@naver.com>
2023-01-18 21:27:47 +0900

    :tada: 시작!

---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Main() {
+  return <div className="bg-slate-500 text-2xl">메인 페이지다~</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./NotFound";
+import Main from "./components/Main";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [{ index: true, element: <Main /> }],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
