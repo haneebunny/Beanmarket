710a1de82e9f3399a8afdc785ead2f4ebd5263e3
haneebunny <re____@naver.com>
2023-01-19 18:11:21 +0900

    :tada: Next

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-=======
-  content: ["./src/**/*.{html,js, jsx, ts, tsx}"],
->>>>>>> b45bbf4fbda626f5b212b533432c8d1f9cd70a6f
   theme: {
     extend: {},
   },
